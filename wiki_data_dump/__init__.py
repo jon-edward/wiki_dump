d9834c508b458cf00ad7e0dcb30a16b0e1a5a3f9
jon_edward <arithmatlic@gmail.com>
2022-04-21 09:41:58 -0400

    line continuation

--- wiki_data_dump/__init__.py
+++ wiki_data_dump/__init__.py
@@ -1,4 +1,5 @@
-"""A library that assists in traversing and downloading from Wikimedia Data Dumps and its mirrors."""
+"""A library that assists in traversing and downloading from
+Wikimedia Data Dumps and its mirrors."""
 
 from wiki_data_dump.core import WikiDump
 from wiki_data_dump.api_response import Wiki, Job, File
