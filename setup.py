a26141653034e3f0514e2128c4100baf697dde32
jon_edward <arithmatlic@gmail.com>
2022-04-21 11:11:41 -0400

    new version - docstrings

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="wiki_data_dump",
-    version="0.1.0",
+    version="0.1.1",
     author="jon-edward",
     author_email="arithmatlic@gmail.com",
     description="A package for traversing and downloading files from Wiki Data Dump mirrors.",
